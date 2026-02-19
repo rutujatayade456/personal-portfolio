a3d8c7199e2eb4ede396821dad658ba382885662
rutujatayade456 <tayaderutuja501@gmail.com>
2026-02-19 18:13:33 +0530

    Update srcipt.js

--- srcipt.js
+++ srcipt.js
@@ -40,4 +40,48 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
     alert("Message Sent Successfully ✅");
     this.reset();
 });
+// Scroll Animation
+window.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
 
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const triggerPoint = window.innerHeight - 100;
+
+        if (sectionTop < triggerPoint) {
+            section.classList.add("show");
+        }
+    });
+});
+// Active Navbar Highlight
+window.addEventListener("scroll", () => {
+    let sections = document.querySelectorAll("section");
+    let navLinks = document.querySelectorAll(".nav-links a");
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector(".nav-links a[href*=" + id + "]").classList.add("active");
+            });
+        }
+    });
+});
+// Animate Skills
+window.addEventListener("scroll", () => {
+    const skills = document.querySelectorAll(".progress");
+
+    skills.forEach(skill => {
+        const position = skill.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight - 100;
+
+        if(position < screenPosition){
+            skill.style.width = skill.getAttribute("style").split(":")[1];
+        }
+    });
+});
